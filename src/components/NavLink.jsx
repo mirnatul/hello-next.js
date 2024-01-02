587f2e48ff479a64b9bbc8cfc971517527ba0e59
mirnatul <www.mirnatul@gmail.com>
2024-01-03 02:39:35 +0600

    get back to  next js again

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -9,6 +9,7 @@ const NavLink = ({ children, href, exact = false, activeClassName, ...props }) =
 
     const path = usePathname();
     const active = exact ? path === href : path.startsWith(href);
+
     // if active is true then give the value and if it is false then it will be filtered by the classNames.js file
     const classes = classNames(props.className, active && activeClassName);
     if (classes) {

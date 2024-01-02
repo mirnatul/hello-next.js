eecee6336b2b1357ccaea944e665a083baf0a626
mirnatul <www.mirnatul@gmail.com>
2024-01-03 03:46:59 +0600

    worked with useParams, useSearchParams and useRouter

--- src/app/blogs/[...segtments]/page.js
+++ src/app/blogs/[...segtments]/page.js
@@ -1,13 +1,27 @@
+'use client'
+
+import { useParams, useSearchParams } from 'next/navigation';
 import React from 'react';
 
-const SingleBlog = ({ params }) => {
+const SingleBlog = ({ params, searchParams }) => {
 
-    console.log(params.segtments);
+    // console.log(params.segtments);
     const [year, id] = params.segtments || [];
-    // {id: 69}
+
+    const params2 = useParams();
+    const searchParams2 = useSearchParams();
+
+    console.log(params2.segtments);
+    console.log(searchParams2.get('title'));
+
+
     return (
         <div>
             single blog {year} for {id}
+            <br />
+            {
+                searchParams.title
+            }
         </div>
     );
 };

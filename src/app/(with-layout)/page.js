42ff3ef51a7e270ae1ef40ac8c1b6a6ed56956a1
mirnatul <www.mirnatul@gmail.com>
2024-01-03 12:05:52 +0600

    basic of image tag

--- src/app/(with-layout)/page.js
+++ src/app/(with-layout)/page.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import image1 from "@/assets/_1fb85d87-1b6f-489d-b5b7-9ba1a88a1595.jpg"
 
 const HomePage = () => {
   return (
     <main>
       <h1>Home page</h1>
+      <Image width={600} height={500} src={image1} alt=""></Image>
     </main>
   )
 }
